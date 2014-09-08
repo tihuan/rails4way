71291c8448618b45e5720ee24ede842f04d53fbc
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-08 15:26:33 -0700

    use DECENT EXPOSURE GEM

---
+++ action_view.rb
@@ -0,0 +1,7 @@
+# Use Decent Exposure gem
+# This example shows how expose declarations can depend on each other
+expose(:client)
+expose(:timesheet) { client.timesheets.find(params[:id]) }
+expose(:timesheet_approval_presenter) do
+  TimesheetApprovalPresenter.new(timesheet, current_user)
+end
