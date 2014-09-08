8b0d7d1147a594dc98a111525dd0bdf007b045ae
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-08 13:08:20 -0700

    complete non-persisted model

--- advanced_ar.rb
+++ advanced_ar.rb
@@ -328,4 +328,22 @@ class Contact
   extend ActiveModel::Translation
   include ActiveModel::Conversion
   include ActiveModel::Validations
+
+  attr_accessor :name, :email, :message
+
+  validates :name, presence: true
+  validates :email,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ },
+    presence: true
+  validates :message, length: { maximum: 1000 }, presence: true
+
+  def initialize(attributes= {} )
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
+  end
 end
