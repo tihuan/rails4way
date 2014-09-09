8948a33a81027f1135891ae0e95ed0ef130458e0
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-08 20:10:48 -0700

    error_messages_for and form.error_messages

--- helpers.rb
+++ helpers.rb
@@ -6,3 +6,11 @@
   .textual
     = form.text_field :first_name
     = form.error_message_on :first_name
+
+# error_message_for(*params)
+# when used with form, use error_messages instead
+= form_for @person do |form|
+  = form.error_messages
+  .text-field
+    = form.label :name, "Name"
+    = form.text_field :name
