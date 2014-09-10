dcd9b1a5636cdf7aebffaf3579c86a34004148f0
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-10 15:23:24 -0700

    modify User to include Devise modules

--- authentication.rb
+++ authentication.rb
@@ -4,3 +4,13 @@ rails generate devise_install
 
 # run Devise generator for User
 rails generate devise User
+
+# Check the migration file and comment out unwatned features
+
+# Go to User model and add modules included in migration
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, and :omniauthable
+  devise :database_authenticatable, :registerable,
+              :recoverable, :rememberable, :trackable, :validatable
+end
