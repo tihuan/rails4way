b68273f04a0b9bd5007de5ae203c8c6d2bbefd57
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-10 12:49:17 -0700

    clean up old session

--- session.rb
+++ session.rb
@@ -7,3 +7,12 @@ config.cache_store = :mem_cache_store
 # config/initializers/session_store.rb
 Example::Application.config.
   session_store ActionDispatch::Session::CacheStore
+
+# Cleaning Up Old Sessions
+# /lib folder
+class SessionMaintenance
+  def self.cleanup(period = 24.hours.ago)
+    session_store = ActiveRecord::SessionStore::Sessions
+    session_store.where('updated_at < ?', period).delete_all
+  end
+end
