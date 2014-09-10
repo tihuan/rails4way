249493add959a23a7d3fb23f308d0eb6f54671b6
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-10 12:39:46 -0700

    tell Rails to use cache_store as underlying session store

--- session.rb
+++ session.rb
@@ -3,3 +3,7 @@
 gem "dalli"
 # config/environments/production.rb
 config.cache_store = :mem_cache_store
+
+# config/initializers/session_store.rb
+Example::Application.config.
+  session_store ActionDispatch::Session::CacheStore
